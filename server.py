b211a33373a4101bbe8c3a6debfe858249034693
lumar0000 <luca.grassi@alessandrinimainardi.edu.it>
2023-12-13 13:50:52 +0100

    socket 1

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5005
+BUFFER_SIZE = 1024
+
+# creazione del socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(SERVER_IP, SERVER_PORT)
+
+print("Server in attesa di messaggi...")
+
+while True:
+    #ricezione dei dati del client
+    data, addr=sock.recvfrom(BUFFER_SIZE)
+    print("Messaggio ricevute del client {addr}: {data.decode()}")
+
+    #invio di una risposta al client
+    reply = "pong"
+    sock.sendto(reply.encode(), addr)
